c4a3c7862fc6bada41bd5c0d162c724afca91ee9
Justin Byun <klofseattle@hotmail.com>
2020-07-10 15:42:50 -0700

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+// components
 
-function App() {
+const App = () => {
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Quiz App
+      <button className="start" onClick={startTrivia}>
+        start
+      </button>
+      <p className="score">Score: </p>
+      <div className="loader"></div>
     </div>
   );
-}
+};
 
 export default App;
